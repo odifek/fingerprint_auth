514236da041ffe11f831b60d9044dc4381a267b9
Odife K <kennedy998@gmail.com>
2017-12-03 23:54:21 +0100

    Added lots of comments

--- authenticate.py
+++ authenticate.py
@@ -15,7 +15,7 @@ DB_FILE = "bio_fp.db"
 def authenticate():
     pyfprint.fp_init()  # Initialize the fingerprint library
     devs = pyfprint.discover_devices()
-    dev = devs[0]
+    dev = devs[0] # Select the first device detected
     dev.open()
 
     # Connect to the database database
@@ -24,27 +24,39 @@ def authenticate():
     gallery = []
 
     print("To gain access, we have to verify you are authorized using the biometric system\n")
+
     with fdb:
         cur = fdb.cursor()
+
+        # Check that the table is already created. if not, it means no enrolment has been made so far
+        # The function will return immediately
         cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='biometric'")
         if not cur.fetchone():
             print("No data in database yet. Please enroll first")
             return
 
+        # Query the database for all entries, storing them in a vector list (users and gallery)
         cur.execute("SELECT username, finger_p FROM biometric")
         for row in cur.fetchall():
             fp_str = row[1].decode('utf8', 'surrogateescape') # Decode binary data from db
             data = pyfprint.pyf.fp_print_data_from_data(fp_str)
             gallery.append(pyfprint.Fprint(data_ptr=data))
             users.append(row[0])
+
+        # After querying the database, proceed to ask for users finger swipe
         print("To proceed, please swipe your RIGHT INDEX finger on the sensor...\n")
+
+        # Verification is done here
+        # Checks to see if the users fingerprint match any of those retrieved from database
         n, fprint, img = dev.identify_finger(gallery)
-        print("Verifying...")
+        print("Authenticating...")
 
     if fprint:
-        print("Verified! {0}".format(users[n]))
+        print("Authenticated! {0}".format(users[n])) # User is authenticated
+        result = True
     else:
-        print("Verification Failed")
+        print("Authentication Failed")  # User not authenticated
+        result = False
 
     # verified, img = dev.verify_finger(fprint)
     # if verified:
@@ -55,5 +67,6 @@ def authenticate():
     # if img is not None:
     dev.close()
     pyfprint.fp_exit()
+    return result
 
 authenticate() # Run the authentication function
--- enroll.py
+++ enroll.py
@@ -1,4 +1,10 @@
-# Create table and insert fingerprint data into database
+#! python3
+
+"""
+Takes care of enrolment of individual fingerprints. The right index finger is used here.
+The biometrics data is stored in the database along with the username of the person
+"""
+
 from __future__ import print_function
 import pyfprint
 import sqlite3
@@ -8,6 +14,7 @@ def capture():
     print("To proceed with your fingerprint enrolment\n")
     username = input("Please enter your username: ")
 
+    # Initialization and selection of fingerprint module
     pyfprint.fp_init()
     devs = pyfprint.discover_devices()
     dev = devs[0]
@@ -15,12 +22,14 @@ def capture():
 
     print("Enrolling...\n\n")
     print("Please swipe your RIGHT INDEX finger on the sensor 5 times")
-    fprint, img = dev.enroll_finger()
+    fprint, img = dev.enroll_finger() # Fingerprint data is retried along with the image which can be used to update a GUI
 
+    # Connect database
     fdb = sqlite3.connect(DB_FILE)
 
+    # Work on the database object
     with fdb:
-        cur = fdb.cursor()
+        cur = fdb.cursor() # The cursor is used by python to address a table in the database
         cur.execute("CREATE TABLE IF NOT EXISTS biometric (username, finger_p BLOB)")
         if fprint is not None:
             b_fp = fprint.data().encode('utf8', 'surrogateescape') # Convert to binary data for onward storage in db
@@ -29,8 +38,10 @@ def capture():
         else:
             print("finger print not properly captured!")
 
+    # Closing formalities
     dev.close()
     pyfprint.fp_exit()
 
 
-capture()
+capture()   # The function is executed here. (Nothing will happen with the above
+# except the function is called in this form!)
